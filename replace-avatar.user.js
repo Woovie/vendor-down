0f8627f887134ac153faa8fcdc060f9c4dfa4e06
Jordan Banasik <woovie@woovie.net>
2025-01-15 13:28:45 -0800

    Create replace-avatar.user.js

---
+++ replace-avatar.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Vendor down avatar replacement
+// @namespace    http://github.com/
+// @version      1.3
+// @description  Replace the avatar image based on a list of usernames of known scam vendors
+// @author       Woovie
+// @match        https://geekhack.org/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/woovie/vendor-down/main/replace-avatar.user.js
+// @downloadURL  https://raw.githubusercontent.com/woovie/vendor-down/main/replace-avatar.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const usernames = ["jchan94", "Jaxxstatic"];
+    const replacementImageUrl = "https://github.com/woovie/vendor-down/blob/main/vendor-down.png";
+
+    const posters = document.querySelectorAll('div.poster');
+
+    posters.forEach(poster => {
+        const usernameElement = poster.querySelector('h4 > a:nth-of-type(2)');
+        if (!usernameElement) return;
+
+        const username = usernameElement.textContent.trim();
+        if (usernames.includes(username)) {
+            const avatarImg = poster.querySelector('img.avatar');
+            if (avatarImg) {
+                avatarImg.src = replacementImageUrl;
+                avatarImg.style.width = "100%";
+            }
+        }
+    });
+})();
